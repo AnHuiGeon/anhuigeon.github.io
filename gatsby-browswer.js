1f02da084bc58e960fb5f268a0527a573975aa87
AnHuiGeon <dndodl@nate.com>
2021-12-14 14:28:40 +0900

    gatsby-browser.js#1

---
+++ gatsby-browswer.js
@@ -0,0 +1,15 @@
+const React = require("react")
+const Index = require("./src/templates/index")
+
+// Logs when the client route changes
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
+
+// Wraps every page in a component
+exports.wrapPageElement = ({ element, props }) => {
+  console.log('element:',element)
+  console.log('props:',props)
+  return <Index {...props}>{element}</Index>
+}
